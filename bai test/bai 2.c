625c29766deff9e5259fe6121a2df5008897e36b
Quangkocode <quanghoang26092007@gmail.com>
2025-09-05 19:21:08 +0700

    Add files via upload

---
+++ bai test/bai 2.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+int main(){
+int isPrime(int n) {
+    if (n < 2) {
+        return 0; 
+    }
+    for (int i = 2; i * i <= n; i++) {
+        if (n % i == 0) {
+            return 0; 
+        }
+    }
+    return 1; 
+}
+    int n;
+    printf("Enter the number of elements in the array: ");
+    scanf("%d", &n);
+    int arr[n];
+    for (int i = 0; i < n; i++) {
+    	printf("Enter element %d: \n", i+1);
+        scanf("%d", &arr[i]);
+    }
+
+    int foundPrime = 0;
+    printf("Prime numbers in the array: ");
+    for (int i = 0; i < n; i++) {
+        if (isPrime(arr[i])) {
+            printf("%d ", arr[i]);
+            foundPrime = 1;
+        }
+    }
+
+    if (foundPrime==0) {
+        printf("No prime number\n");
+    }
+}
